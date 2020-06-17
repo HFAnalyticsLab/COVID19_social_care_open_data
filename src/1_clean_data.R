01ea5310451254434a7ad98970cafa88114a2532
Emma Vestesson <emma.vestesson@gmail.com>
2020-06-18 00:07:38 +0200

    Clean table 1

--- src/1_clean_data.R
+++ src/1_clean_data.R
@@ -64,4 +64,41 @@ home_care <- home_care_table_8 %>%
 saveRDS(home_care, here::here('data', 'home_care.rds'))
 
 
+## Table 1 -----
+
+home_care_table_1 <- read_excel(here::here('data', 'original data', "deathsinvolvingcovid19inthecaresectordataset.xlsx"),
+                                sheet = "Table 1 ", skip = 2)
+home_care_table_1 <- home_care_table_1 %>% 
+  remove_empty( c("rows", "cols")) %>% 
+  clean_names()
+
+names(home_care_table_1)[1] <- 'date'
+
+home_care_table_1_transposed <- home_care_table_1 %>% 
+  t() %>% 
+  as.data.frame() %>% 
+  rownames_to_column(var = 'top_header') %>% 
+  mutate(top_header=ifelse(str_detect(top_header,'x[0-9]'), NA, top_header)) %>% 
+  fill(top_header) 
+eng_ons <- home_care_table_1_transposed %>% 
+  filter(top_header=='england_ons_data' | top_header=='date') %>% 
+  select(-top_header) %>% 
+  t() %>% 
+  as.data.frame() %>% 
+  row_to_names(1) %>% 
+  clean_names() %>% 
+  mutate(across(everything(), ~as.numeric(as.character(.)))) %>% 
+  mutate(date=as.Date(date, origin = "1899-12-30")) %>% 
+  filter(!is.na(all_deaths)) %>% 
+  mutate(deaths_involving_covid_19=replace_na(deaths_involving_covid_19, 0),
+           non_covid=all_deaths-deaths_involving_covid_19) %>% 
+  rename(covid=deaths_involving_covid_19) %>% 
+  mutate(covid_daily=ifelse(date=='2019-12-28',covid, covid-lag(covid,1)),
+         non_covid_daily=ifelse(date=='2019-12-28',non_covid,non_covid-lag(non_covid,1)), 
+         non_covid_daily_2019= ifelse(date=='2019-12-28',x2019_comparison,x2019_comparison-lag(x2019_comparison,1)) 
+         )
+
+## Save data
+
+saveRDS(eng_ons, here::here('data', 'care_home_deaths_england.rds'))
 
