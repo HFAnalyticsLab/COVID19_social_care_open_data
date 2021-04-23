4ecbb831653496095d0d06ab93f55c14729b3ba7
fiona-grimm <Fiona.grimm@health.org.uk>
2021-04-23 10:28:17 +0100

    Add annotated version of mortality graph

--- sprint_2/4_visualising_carehome_residents_deaths_data.R
+++ sprint_2/4_visualising_carehome_residents_deaths_data.R
@@ -73,7 +73,7 @@ df %>%
   labs(x= "", y="")+
   scale_y_continuous(limits = c(0,11000), breaks = seq(0, 11000, by = 2000))+
   theme(plot.title = element_text(size=16),legend.text=element_text(size=12),
-        axis.text.x=element_text(size=11),axis.text.y=element_text(size=11), plot.caption = element_markdown(hjust = 0, size=10))
+        axis.text.x=element_text(size=11),axis.text.y=element_text(size=11))
 
   ggsave(here::here('sprint_2','graphs', 'care_home_residents_deaths_ONS.png'),dpi=300,
          width = 10, height = 6.5) 
@@ -83,4 +83,48 @@ df %>%
     filter(week_start>as.Date('2020-03-06')) %>% 
     select(week_start, ch_deaths_noncovid_england, ch_covid_deaths_england, ch_deaths_all_avg_2015_2019_england) %>% 
     write_csv(here::here('sprint_2','graphs', 'care_home_residents_deaths_ONS.csv'))
+  
+
+  # Annotated version of the graph
+  
+ df %>% 
+    filter(week_start>as.Date('2020-03-06')) %>% 
+    select(week_start, ch_deaths_noncovid_england, ch_covid_deaths_england) %>% 
+    pivot_longer(-week_start, names_to="variable", values_to="count") %>% 
+    mutate(variable = case_when(variable == "ch_deaths_noncovid_england" ~ "NON-COVID-19",
+                                variable == "ch_covid_deaths_england" ~  "COVID-19")) %>% 
+    ggplot(aes(x = week_start, y = count, fill = variable))+
+    geom_bar(position="stack", stat="identity") +
+    geom_line(data = df[df$week_start>as.Date('2020-03-06'),], 
+              aes(x = week_start, y = ch_deaths_all_avg_2015_2019_england, fill = NULL, color = "average 2015-2019"),  stat="identity", size=1) +
+    scale_x_date(breaks = seq(as.Date('2020-03-07'),max(df$week_start),by="4 weeks"),
+                 date_labels = '%d %b %g') +
+    
+    annotate("segment", x=as.Date("2020-03-11"), xend=as.Date("2020-03-11"), y=0, yend=9000, linetype="dashed", size=1, colour="black")+
+    annotate("segment", x=as.Date("2020-06-15"), xend=as.Date("2020-06-15"), y=0, yend=9000, linetype="dashed", size=1, colour="black")+
+    annotate("text",x=as.Date("2020-03-11"), y=9800, label=lab_firstwave, size=3.2, colour="black",hjust=0)+
+    annotate("segment", x=as.Date("2020-09-02"), xend=as.Date("2020-09-02"), y=0, yend=9000, linetype="dashed", size=1, colour="black")+
+    annotate("text",x=as.Date("2020-09-02"), y=9800, label=lab_secondwave, size=3.2, colour="black", hjust=0)+
+    annotate("rect", xmin = as.Date("2020-03-11"), xmax =as.Date("2020-06-15"), ymin = 0, ymax = 9000, alpha = .1,fill = "grey20")+
+    annotate("rect", xmin = as.Date("2020-09-02"), xmax =as.Date("2021-03-31"), ymin = 0, ymax = 9000, alpha = .1,fill = "grey20")+
+    scale_fill_THF()+
+    theme_THF() +
+    scale_colour_manual(name="", values=c("average 2015-2019" = "grey20")) +  
+    labs(x= "", y="", title = "Deaths of care home residents in England, by week reported",
+    caption = "Source: ONS Care home resident deaths registered in England and Wales, provisional, 2021; ONS Weekly provisional figures<br>
+    of care home resident deaths registered in England and Wales, 2020; ONS Deaths registered weekly in England and Wales,<br>
+    provisional. Excess deaths in care homes during the second wave of COVID-19 might be an underestimate, as the average<br>
+    number of deaths for care home residents between 2015 and 2019 was not available for week 53. Weeks commencing on Saturdays.<br>
+    At the time of writing data was available up to 2 April 2021.")+
+    scale_y_continuous(limits = c(0,11000), breaks = seq(0, 11000, by = 2000))+
+    theme(plot.title = element_text(size=16),
+          legend.text=element_text(size=12),
+          axis.text.x=element_text(size=11),
+          axis.text.y=element_text(size=11), 
+          plot.caption = element_markdown(hjust = 0, size=10),
+          legend.margin=margin(0,0,0,0),
+          legend.box.margin=margin(15,5,-25,-10))
+  
+ggsave(here::here('sprint_2','graphs', 'care_home_residents_deaths_ONS_annotated.png'),dpi=300,
+         width = 10, height = 8) 
   
